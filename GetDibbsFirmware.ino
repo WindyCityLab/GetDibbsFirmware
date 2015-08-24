83dd67ca480193faac951cb984882b7c2012b9a1
Kevin McQuown <kevinmcquown@Kevins-MacBook-Pro.local>
2015-08-24 14:26:59 -0500

    updates

--- GetDibbsFirmware.ino
+++ GetDibbsFirmware.ino
@@ -11,6 +11,8 @@ LiquidTWI2 lcd(0x20);
 char blynkAuthCode[] = "ca8de794d7534659b2b4b5c995333f3a";
 LightMatrixManager matrix;
 
+//uint8_t hour, week, day;
+
 unsigned int localPort = 2390;
 IPAddress timeServerIP; // time.nist.gov NTP server address
 const char* ntpServerName = "time.nist.gov";
@@ -21,27 +23,23 @@ WiFiUDP udp;
 
 void getTime()
 {
-  while (WiFi.status() != WL_CONNECTED) {
-    Serial.println("not connected?");
-  }
-  Serial.println("yea, connected");
   udp.begin(localPort);
   //get a random server from the pool
   WiFi.hostByName(ntpServerName, timeServerIP);
 
-  Serial.println(timeServerIP);
-  
+  lcd.print(timeServerIP);
+
   sendNTPpacket(timeServerIP); // send an NTP packet to a time server
   // wait to see if a reply is available
 
   int cb = udp.parsePacket();
   while (!cb) {
-    Serial.println("no packet yet");
+    lcd.print("no packet yet");
     Blynk.run();
     cb = udp.parsePacket();
   }
-  Serial.print("packet received, length=");
-  Serial.println(cb);
+  lcd.print("packet received, length=");
+  lcd.println(cb);
   // We've received a packet, read the data from it
   udp.read(packetBuffer, NTP_PACKET_SIZE); // read the packet into the buffer
 
@@ -53,34 +51,34 @@ void getTime()
   // combine the four bytes (two words) into a long integer
   // this is NTP time (seconds since Jan 1 1900):
   unsigned long secsSince1900 = highWord << 16 | lowWord;
-  Serial.print("Seconds since Jan 1 1900 = " );
-  Serial.println(secsSince1900);
+  //Serial.print("Seconds since Jan 1 1900 = " );
 
   // now convert NTP time into everyday time:
-  Serial.print("Unix time = ");
   // Unix time starts on Jan 1 1970. In seconds, that's 2208988800:
   const unsigned long seventyYears = 2208988800UL;
   // subtract seventy years:
   unsigned long epoch = secsSince1900 - seventyYears;
   // print Unix time:
-  Serial.println(epoch);
+  lcd.clear();
+  lcd.print(epoch);
 
-
+  //hour = (epoch  % 86400L) / 3600;
+  
   // print the hour, minute and second:
-  Serial.print("The UTC time is ");       // UTC is the time at Greenwich Meridian (GMT)
-  Serial.print((epoch  % 86400L) / 3600); // print the hour (86400 equals secs per day)
-  Serial.print(':');
-  if ( ((epoch % 3600) / 60) < 10 ) {
-    // In the first 10 minutes of each hour, we'll want a leading '0'
-    Serial.print('0');
-  }
-  Serial.print((epoch  % 3600) / 60); // print the minute (3600 equals secs per minute)
-  Serial.print(':');
-  if ( (epoch % 60) < 10 ) {
-    // In the first 10 seconds of each minute, we'll want a leading '0'
-    Serial.print('0');
-  }
-  Serial.println(epoch % 60); // print the second
+  //  Serial.print("The UTC time is ");       // UTC is the time at Greenwich Meridian (GMT)
+  //  Serial.print((epoch  % 86400L) / 3600); // print the hour (86400 equals secs per day)
+  //  Serial.print(':');
+  //  if ( ((epoch % 3600) / 60) < 10 ) {
+  //    // In the first 10 minutes of each hour, we'll want a leading '0'
+  //    Serial.print('0');
+  //  }
+  //  Serial.print((epoch  % 3600) / 60); // print the minute (3600 equals secs per minute)
+  //  Serial.print(':');
+  //  if ( (epoch % 60) < 10 ) {
+  //    // In the first 10 seconds of each minute, we'll want a leading '0'
+  //    Serial.print('0');
+  //  }
+  //  Serial.println(epoch % 60); // print the second
 }
 
 void initializeLCD()
@@ -116,14 +114,12 @@ void initializeBlynk()
   lcd.print("Connected!");
 }
 void setup() {
-  Serial.begin(9600);
   matrix.begin(2);
   matrix.clearDisplay();
   initializeLCD();
   initializeBlynk();
- // getTime();
+  getTime();
   initializeMatrix();
-
 }
 
 BLYNK_WRITE(5) // Increment Day
@@ -157,7 +153,7 @@ void loop() {
 
   Blynk.run();
 }
-// send an NTP request to the time server at the given address
+
 unsigned long sendNTPpacket(IPAddress& address)
 {
   Serial.println("sending NTP packet...");
@@ -177,9 +173,9 @@ unsigned long sendNTPpacket(IPAddress& address)
 
   // all NTP fields have been given values, now
   // you can send a packet requesting a timestamp:
-  //  udp.beginPacket(address, 123); //NTP requests are to port 123
-  //  udp.write(packetBuffer, NTP_PACKET_SIZE);
-  //  udp.endPacket();
+  udp.beginPacket(address, 123); //NTP requests are to port 123
+  udp.write(packetBuffer, NTP_PACKET_SIZE);
+  udp.endPacket();
 }
 
 
